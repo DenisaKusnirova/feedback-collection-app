71d33b79779c67e20e06f2689ef76af2fef17be8
Deniska Kusnirova <deniskakusnirova@gmail.com>
2020-01-02 14:06:14 +0100

    Fix gitignore

--- config/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  googleClientID: '734885820073-5otnuia8c1o96chjclsuhi764b25eb3h.apps.googleusercontent.com',
-  googleClientSecret: 'PIH8qrsMLdOhUEq8YcFsJdnw',
-  mongoURI: 'mongodb+srv://p00gz:1cerCLi9M1JaYxeh@feedback-collection-app-tfzln.mongodb.net/test?retryWrites=true&w=majority',
-  cookieKey: 'fkkepwfjewfjerudfgvbfgfdffggthgrkoerkewpdfdfspdapff',
-};
