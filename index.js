4bb6c57f3be9a1c6a0cf18364bfd45d363549e18
Deniska Kusnirova <deniskakusnirova@gmail.com>
2020-01-10 19:15:09 +0100

    Handle routing

--- index.js
+++ index.js
@@ -30,5 +30,16 @@ app.use(passport.session());
 authRoutes(app);
 billingRoutes(app);
 
+// if any get request comes in that requires anything that's not set up in this file, try to look inside client/build
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  // catch-all case, all previous possibilities failed, return index.html file
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
