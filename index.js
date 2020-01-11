9401679f868758630b90645986febc1b30fb9a67
Deniska Kusnirova <deniskakusnirova@gmail.com>
2020-01-11 12:21:07 +0100

    Create Recipient and Survey schema

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const cookieSession = require("cookie-session");
 const passport = require("passport");
 const bodyParser = require("body-parser");
 require("./models/User");
+require("./models/Survey");
 require("./services/passport");
 
 // Connect mongoose to mongodb
